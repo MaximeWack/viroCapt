ed437d3afd04ce894007cd24efcd9179d3c51626
Maxime Wack <maximewack@free.Fr>
2019-06-06 15:26:49 +0200

    Add UI elements

--- inst/visu/ui.R
+++ inst/visu/ui.R
@@ -1,8 +1,25 @@
 library(tidyverse)
+library(HPVcap)
 library(shiny)
 
-ui <- fluidPage(titlePanel("HPV integration pipeline results"),
-                fileInputr
-
-
-
+ui <- bootstrapPage(
+        sidebarPanel(
+          titlePanel("HPV integration pipeline results"),
+          inputPanel(fileInput("sam", "SAM file", accept = "*.sam"),
+                     fileInput("summ", "Blat summary file", accept = "*_summary.tsv")),
+          inputPanel(selectizeInput("genotype",
+                                    "Genotypes",
+                                    "",
+                                    multiple = T),
+                     selectizeInput("scores",
+                                    "Scores",
+                                    "",
+                                    multiple = T),
+                     selectizeInput("chrs",
+                                    "Chromosomes",
+                                    "",
+                                    multiple = T),
+                     sliderInput("nreads", "Nombre de reads minimum", 0, 0, 0),
+                     sliderInput("match", "Score minimal de matching", 0, 0, 0))),
+        mainPanel(plotOutput("plot"),
+                  DT::dataTableOutput("table")))
