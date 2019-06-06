6429b0713cd8254799f8e829a67400c71868f11c
Maxime Wack <maximewack@free.Fr>
2019-06-06 15:28:03 +0200

    Load sam file

--- inst/visu/server.R
+++ inst/visu/server.R
@@ -4,4 +4,27 @@ library(shiny)
 
 server <- function(input, output, session)
 {
+  # Read and parse the sam file
+  sam <- reactive({
+    req(input$sam)
+
+    withProgress({
+      incProgress(message = "Reading sam file")
+      input$sam$datapath %>%
+        HPVcap:::read_sam() -> sam
+
+      incProgress(message = "Parsing CIGARs")
+      sam %>%
+        mutate(parsed = cigar %>% HPVcap:::parse_cigar()) %>%
+        unnest -> sam
+
+      incProgress(message = "Computing nucleotide read depth")
+      sam %>%
+        HPVcap:::read_depth() -> sam
+
+      incProgress(message = "Downsampling for displaying")
+      sam %>%
+        HPVcap:::downsample()
+    })
+  })
 }
