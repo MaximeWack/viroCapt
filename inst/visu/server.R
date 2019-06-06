ed4b53c5b649dc24c15bb3a8e4c54d0af166ecc3
Maxime Wack <maximewack@free.Fr>
2019-06-06 15:29:49 +0200

    Set UI values based on loaded files

--- inst/visu/server.R
+++ inst/visu/server.R
@@ -4,6 +4,16 @@ library(shiny)
 
 options(shiny.maxRequestSize = 1024 ^ 3)
 
+UIlabels <- function(variable)
+{
+  variable %>%
+    fct_infreq %>%
+    summary -> varsumm
+
+  names(varsumm) %>%
+    setNames(str_c(names(varsumm), " (", varsumm, ")"))
+}
+
 server <- function(input, output, session)
 {
   # Read and parse the sam file
@@ -37,4 +47,21 @@ server <- function(input, output, session)
     input$summ$datapath %>%
       HPVcap:::read_summary()
   })
+
+  # Set UI values
+  observe({
+    sam() %>%
+      group_by(genotype) %>%
+      summarise(n = max(n)) -> genotypes
+
+    genotypes$genotype %>%
+      setNames(str_c(genotypes$genotype, " (", genotypes$n, ")")) -> genotypes
+
+    updateSelectizeInput(session, "genotype", choices = genotypes) #, selected = genotypes[1])
+  })
+
+  observe(updateSelectizeInput(session, "scores", choices = summ_blat()$quality %>% UIlabels))
+  observe(updateSelectizeInput(session, "chrs", choices = summ_blat()$chr %>% UIlabels))
+  observe(updateSliderInput(session, "nreads", min = summ_blat()$n %>% min, max = summ_blat()$n %>% max))
+  observe(updateSliderInput(session, "match", min = summ_blat()$match %>% min, max = summ_blat()$match %>% max))
 }
