6ebe26024a964b61064723f56b473a49ccd0beb5
Maxime Wack <maximewack@free.Fr>
2021-12-01 21:42:41 +0100

    Allow visu() to pass args to shiny::runApp

--- R/visu.R
+++ R/visu.R
@@ -1,11 +1,11 @@
 #' Shiny application to interactively visualize results from the pipeline
 #'
 #' @export
-visu <- function() {
+visu <- function(...) {
   appDir <- system.file("visu", package = "viroCapt")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `viroCapt`.", call. = FALSE)
   }
 
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(appDir, display.mode = "normal", ...)
 }
