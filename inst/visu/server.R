216c1fbc08a67067e45ca561e22c3c7e8a56c59d
Maxime Wack <maximewack@free.Fr>
2021-12-02 01:21:30 +0100

    Reorganise and comment
    Add hierarchised comments
    Separate plot and table creation

--- inst/visu/server.R
+++ inst/visu/server.R
@@ -17,51 +17,28 @@ UIlabels <- function(variable)
 server <- function(input, output, session)
 {
   # Read the visualisation object
+
   visu <- reactive({
     req(input$visu)
 
     readRDS(input$visu$datapath)
   })
 
-  # Extract the sequencing depth object
+  # Plot
+
+  ## Extract the sequencing depth object
   sam <- reactive({
     req(visu())
 
     visu()$profile
   })
 
-  # Extract the blat summary
-  summ_blat_file <- reactive({
-    req(visu())
-
-    visu()$summary %>%
-      dplyr::mutate(quality = quality %>% forcats::fct_drop())
-  })
-
-  # Set UI values
-  observe({
-    sam() %>%
-      dplyr::group_by(genotype) %>%
-      dplyr::summarise(n = max(n)) %>%
-      dplyr::mutate(genotype = genotype %>% as.character) -> genotypes
-
-    genotypes$genotype %>%
-      stats::setNames(paste0(genotypes$genotype, " (", genotypes$n, ")")) -> genotypes
-
-    updateSelectizeInput(session, "genotype", choices = genotypes, selected = genotypes[1])
-  })
-
-  observe({
-    updateSelectizeInput(session, "scores", choices = summ_blat_file()$quality %>% UIlabels)
-    updateSelectizeInput(session, "chrs", choices = summ_blat_file()$chr %>% UIlabels)
-    updateSliderInput(session, "nreads", min = summ_blat_file()$n %>% min, max = summ_blat_file()$n %>% max)
-    updateSliderInput(session, "match", min = summ_blat_file()$match %>% min, max = summ_blat_file()$match %>% max)
-  })
-
+  ## Downsample the depth information
   downsampled_depths <- reactive({
     sam() %>% viroCapt:::downsample(10)
   })
 
+  ## Filter the depths for the selected genotypes
   depths <- reactive({
     req(input$genotype)
 
@@ -69,25 +46,12 @@ server <- function(input, output, session)
       dplyr::filter(genotype %in% input$genotype)
   })
 
-  summ_blat <- reactive({
-    summ_blat_file() %>%
-      dplyr::filter(genotype %in% input$genotype,
-                    n >= input$nreads,
-                    match >= input$match) -> summ
-
-    if (! input$chrs %>% is.null)
-      summ %>% dplyr::filter(chr %in% input$chrs) -> summ
-
-    if (! input$scores %>% is.null)
-      summ %>% dplyr::filter(quality %in% input$scores) -> summ
-
-    summ
-  })
-
+  ## Base depth plot
   localplot <- reactive({
     viroCapt:::ggplot_depth(depths())
   })
 
+  ## Output the annotated plot
   output$plot <- plotly::renderPlotly({
     max(depths()$n) -> max_n
 
@@ -109,6 +73,33 @@ server <- function(input, output, session)
       theme_classic()
   })
 
+  # Table
+
+  ## Extract the blat summary
+  summ_blat_file <- reactive({
+    req(visu())
+
+    visu()$summary %>%
+      dplyr::mutate(quality = quality %>% forcats::fct_drop())
+  })
+
+  ## Filter the blat summary results on genotype, chr, nreads, score, and match
+  summ_blat <- reactive({
+    summ_blat_file() %>%
+      dplyr::filter(genotype %in% input$genotype,
+                    n >= input$nreads,
+                    match >= input$match) -> summ
+
+    if (! input$chrs %>% is.null)
+      summ %>% dplyr::filter(chr %in% input$chrs) -> summ
+
+    if (! input$scores %>% is.null)
+      summ %>% dplyr::filter(quality %in% input$scores) -> summ
+
+    summ
+  })
+
+  ## Output the table
   output$table <- DT::renderDataTable(
   {
     summ_blat() %>%
@@ -121,4 +112,26 @@ server <- function(input, output, session)
                  fixedHeader = T),
   filter = "top",
   extensions = "Buttons")
+
+  # Update UI values
+
+  observe({
+    sam() %>%
+      dplyr::group_by(genotype) %>%
+      dplyr::summarise(n = max(n)) %>%
+      dplyr::mutate(genotype = genotype %>% as.character) -> genotypes
+
+    genotypes$genotype %>%
+      stats::setNames(paste0(genotypes$genotype, " (", genotypes$n, ")")) -> genotypes
+
+    updateSelectizeInput(session, "genotype", choices = genotypes, selected = genotypes[1])
+  })
+
+  observe({
+    updateSelectizeInput(session, "scores", choices = summ_blat_file()$quality %>% UIlabels)
+    updateSelectizeInput(session, "chrs", choices = summ_blat_file()$chr %>% UIlabels)
+    updateSliderInput(session, "nreads", min = summ_blat_file()$n %>% min, max = summ_blat_file()$n %>% max)
+    updateSliderInput(session, "match", min = summ_blat_file()$match %>% min, max = summ_blat_file()$match %>% max)
+  })
+
 }
